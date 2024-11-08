0458ab2e50c9d28e6c80b3afa00b9d309f0209b4
jackson36067 <jacksonn36067@gmail.com>
2024-11-08 22:52:03 +0800

    菜单组件新增功能实现

--- src/apis/menu.ts
+++ src/apis/menu.ts
@@ -1,9 +1,27 @@
 import httpInstance from "@/utils/http";
 
+/**
+ * 获取菜单集合
+ * @param params
+ * @returns
+ */
 export const getMenuListAPI = (params: object) => {
   return httpInstance({
     method: "get",
     url: "menu/list",
     params,
+  });
+};
+
+/**
+ * 新增菜单
+ * @param data
+ * @returns
+ */
+export const addMenuAPI = (data: object) => {
+  return httpInstance({
+    method: "post",
+    url: "menu/add",
+    data,
   });
 };
--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { getMenuListAPI } from "@/apis/menu";
+import { addMenuAPI, getMenuListAPI } from "@/apis/menu";
+import SvgIcon from "@/components/svg/svgIcon.vue";
 import { formatDateForBackend, formatLocalDateTime } from "@/utils/dateFormat";
 import {
   Delete,
@@ -10,6 +11,7 @@ import {
   RefreshRight,
   Search,
 } from "@element-plus/icons-vue";
+import { ElMessage } from "element-plus";
 import { inject, onMounted, ref, computed } from "vue";
 
 const collapse = inject("collapse");
@@ -50,6 +52,19 @@ const formatMenuListData = (data: Array<object>) => {
   });
 };
 
+// 将得到的菜单改成Tree展示所需类型
+const formatMenuTreeData = (data: Array<object>) => {
+  return data.map((item: any) => {
+    return {
+      id: item.id,
+      label: item.title,
+      children: item.menuListVOList
+        ? formatMenuTreeData(item.menuListVOList)
+        : [], // 递归转换子部门
+    };
+  });
+};
+
 // 查询参数对象
 const queryParams = computed(() => ({
   title: form.value.title,
@@ -58,10 +73,12 @@ const queryParams = computed(() => ({
 }));
 
 const menuTableData = ref([]);
+const menuTreeData = ref([]);
 const getMenuList = async () => {
   // 获取menu菜单信息
   const res = await getMenuListAPI(queryParams.value);
   menuTableData.value = formatMenuListData(res.data);
+  menuTreeData.value = formatMenuTreeData(res.data);
 };
 onMounted(() => {
   getMenuList();
@@ -140,6 +157,149 @@ const shortcuts = [
     },
   },
 ];
+
+// 新增参数
+const addForm = ref({
+  type: 0, // 菜单类型：目录、菜单、按钮
+  icon: "", // 菜单图标
+  iFrame: false, // 外链菜单
+  cached: false, // 菜单缓存
+  hidden: true, // 菜单可见
+  title: "", // 菜单标题
+  permission: "", // 权限标识
+  path: "", // 路由地址
+  name: "", // 组件名称
+  component: "", // 组件路径
+  menuSort: 999, // 菜单排序
+  pid: "", // 上级类别
+});
+
+// 表格校验规格
+const rules = {
+  title: [{ required: true, trigger: "blur", message: "请输入标题" }],
+  path: [{ required: true, trigger: "blur", message: "请输入地址" }],
+};
+
+// 图标列表
+const iconList = ref<Array<string>>([
+  "alipay",
+  "anq",
+  "app",
+  "backup",
+  "blog",
+  "chain",
+  "chart",
+  "codeConsole",
+  "dashboard",
+  "database",
+  "date",
+  "deploy",
+  "dept",
+  "dev",
+  "develop",
+  "dictionary",
+  "doc",
+  "download",
+  "edit",
+  "education",
+  "email",
+  "error",
+  "exit-fullscreen",
+  "fullscreen",
+  "fwb",
+  "github",
+  "gonggao",
+  "grid",
+  "icon",
+  "image",
+  "index",
+  "international",
+  "ipvisits",
+  "java",
+  "layout",
+  "link",
+  "list",
+  "lock",
+  "log",
+  "login",
+  "markdown",
+  "menu",
+  "message",
+  "mnt",
+  "money",
+  "monitor",
+  "nested",
+  "password",
+  "people",
+  "peoples",
+  "permission",
+  "phone",
+  "qiniu",
+  "quanpin",
+  "redis",
+  "refresh",
+  "role",
+  "search",
+  "server",
+  "shopping",
+  "size",
+  "skill",
+  "source",
+  "sqlMonitor",
+  "Steve-Jobs",
+  "swagger",
+  "sys-tools",
+  "system",
+  "system1",
+  "tab",
+  "theme",
+  "timing",
+  "tools",
+  "tree-table",
+  "tree",
+  "unlock",
+  "user",
+  "user1",
+  "validCode",
+  "visits",
+  "web",
+  "wechat",
+  "weixin",
+  "zujian",
+]);
+
+// 新增操作
+// 新增对话框可见性
+const dialogAddTableVisible = ref(false);
+
+// 图标列表是否展示
+const iconListVisable = ref(false);
+// 选择图标后更换图标
+const selectMenuIcon = (icon: string) => {
+  addForm.value.icon = icon;
+  iconListVisable.value = false;
+};
+
+// 点击新增按钮让dialog可见
+const showAddDialog = () => {
+  dialogAddTableVisible.value = true;
+};
+
+// 新增表格dom对象
+const addFomrRef = ref(null);
+// 新增菜单
+const submitFormAndAddMenu = () => {
+  addFomrRef.value.validate(async (valid) => {
+    if (valid) {
+      await addMenuAPI(addForm.value);
+      getMenuList();
+      dialogAddTableVisible.value = false;
+      ElMessage({ type: "success", message: "新增成功" });
+    } else {
+      ElMessage({ type: "info", message: "请填写相应内容" });
+    }
+  });
+};
 </script>
 <template>
   <div class="body" :class="{ left: collapse }">
@@ -195,6 +355,7 @@ const shortcuts = [
           <el-button
             :icon="Plus"
             style="background-color: #000; color: #fff; font-size: 12px"
+            @click="showAddDialog"
           >
             新增
           </el-button>
@@ -238,14 +399,14 @@ const shortcuts = [
           :data="menuTableData"
           style="width: 100%"
           row-key="id"
-          border
           v-loading="tableLoad"
           :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
         >
+          <el-table-column type="selection" width="55" />
           <el-table-column prop="title" label="菜单标题" align="center" />
           <el-table-column prop="icon" label="图标" align="center">
             <template #default="{ row }">
-              <SvgIcon :name="row.icon" />
+              <SvgIcon :name="row.icon" color="#a8abb2" />
             </template>
           </el-table-column>
           <el-table-column prop="menuSort" label="排序" align="center" />
@@ -265,6 +426,160 @@ const shortcuts = [
       </div>
     </div>
   </div>
+  <el-dialog
+    title="新增菜单"
+    v-model="dialogAddTableVisible"
+    width="600px"
+    :close-on-click-modal="false"
+  >
+    <el-form
+      :model="addForm"
+      label-width="100px"
+      :inline="true"
+      ref="addFomrRef"
+      :rules="rules"
+    >
+      <el-form-item label="菜单类型">
+        <el-radio-group v-model="addForm.type">
+          <el-radio-button label="目录" :value="0"></el-radio-button>
+          <el-radio-button label="菜单" :value="1"></el-radio-button>
+          <el-radio-button label="按钮" :value="2"></el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 菜单图标 -->
+      <el-form-item
+        v-if="addForm.type !== 2"
+        label="菜单图标"
+        style="width: 100%"
+      >
+        <el-input
+          v-model="addForm.icon"
+          placeholder="点击选择图标"
+          @focus="iconListVisable = true"
+        >
+          <template #prefix>
+            <SvgIcon
+              :name="addForm.icon ? addForm.icon : 'search'"
+              color="#a8abb2"
+              width="10px"
+              height="10px"
+            />
+          </template>
+        </el-input>
+        <el-scrollbar
+          style="
+            max-height: 300px;
+            margin-top: 10px;
+            width: 100%;
+            border: 1px solid #a8abb2;
+            border-radius: 5px;
+          "
+          v-if="iconListVisable"
+        >
+          <span
+            v-for="(item, index) in iconList"
+            :key="index"
+            class="addIcon"
+            @click="selectMenuIcon(item)"
+          >
+            <SvgIcon :name="item" style="display: inline" color="#a8abb2" />
+            {{ item }}
+          </span>
+        </el-scrollbar>
+      </el-form-item>
+
+      <!-- 外链菜单 -->
+      <el-form-item v-if="addForm.type !== 2" label="外链菜单">
+        <el-radio-group v-model="addForm.iFrame">
+          <el-radio-button label="是" :value="true"></el-radio-button>
+          <el-radio-button label="否" :value="false"></el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 菜单缓存 -->
+      <el-form-item v-if="addForm.type === 1" label="菜单缓存">
+        <el-radio-group v-model="addForm.cached">
+          <el-radio-button label="是" :value="true"></el-radio-button>
+          <el-radio-button label="否" :value="false"></el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 菜单可见 -->
+      <el-form-item v-if="addForm.type !== 2" label="菜单可见">
+        <el-radio-group v-model="addForm.hidden">
+          <el-radio-button label="是" :value="true"></el-radio-button>
+          <el-radio-button label="否" :value="false"></el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+
+      <!-- 菜单标题 -->
+      <el-form-item label="菜单标题" style="width: 100%" prop="title">
+        <el-input v-model="addForm.title" placeholder="菜单标题"></el-input>
+      </el-form-item>
+
+      <!-- 权限标识 -->
+      <el-form-item v-if="addForm.type !== 0" label="权限标识">
+        <el-input
+          v-model="addForm.permission"
+          placeholder="权限标识"
+        ></el-input>
+      </el-form-item>
+
+      <!-- 路由地址 -->
+      <el-form-item
+        v-if="addForm.type !== 2"
+        label="路由地址"
+        prop="path"
+        style="width: 44%"
+      >
+        <el-input v-model="addForm.path" placeholder="路由地址"></el-input>
+      </el-form-item>
+
+      <!-- 组件名称 -->
+      <el-form-item v-if="addForm.type === 1" label="组件名称">
+        <el-input
+          v-model="addForm.name"
+          placeholder="匹配组件内Name字段"
+        ></el-input>
+      </el-form-item>
+
+      <!-- 组件路径 -->
+      <el-form-item v-if="addForm.type === 1" label="组件路径">
+        <el-input v-model="addForm.component" placeholder="组件路径"></el-input>
+      </el-form-item>
+
+      <!-- 菜单排序 -->
+      <el-form-item label="菜单排序">
+        <el-input-number
+          v-model="addForm.menuSort"
+          :min="1"
+          controls-position="right"
+        ></el-input-number>
+      </el-form-item>
+
+      <!-- 上级类别 -->
+      <el-form-item label="上级类别" style="width: 100%">
+        <el-tree-select
+          v-model="addForm.pid"
+          :data="menuTreeData"
+          filterable
+          style="width: 240px"
+          :render-after-expand="false"
+          check-strictly
+          :props="{ label: 'label', value: 'id', children: 'children' }"
+          placeholder="选择上级类目"
+        />
+      </el-form-item>
+    </el-form>
+
+    <template v-slot:footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogAddTableVisible = false">取消</el-button>
+        <el-button type="primary" @click="submitFormAndAddMenu">确认</el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 <style scoped lang="scss">
 .body {
@@ -328,6 +643,33 @@ const shortcuts = [
     }
   }
 }
+.custom-tabs ::v-deep {
+  box-sizing: border-box;
+  font-size: 16px;
+  height: 26px;
+  margin: 28px 0 0 10px;
+}
+.custom-tabs ::v-deep .el-tabs__header {
+  background-color: #fff;
+  color: #000;
+}
+.custom-tabs ::v-deep .el-tabs__item.is-active {
+  color: #fff;
+  background-color: #000;
+  border-radius: 5px;
+  overflow: hidden;
+}
+
+.addIcon {
+  display: inline-block;
+  width: 104px;
+  height: 30px;
+  margin-left: 40px;
+  vertical-align: top;
+  cursor: pointer;
+  overflow: hidden;
+}
+
 .left {
   left: 59px;
   width: calc(100% - 59px);
